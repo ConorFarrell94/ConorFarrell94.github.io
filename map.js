1f333b5ed4f7dc2cdeb262cc28561270934754da
Conor Farrell <conorfarrell187@gmail.com>
2022-12-09 21:00:10 +0000

    formatting

--- map.js
+++ map.js
@@ -1,40 +1,37 @@
-
 let webhook_url =
-"https://eu-west-1.aws.data.mongodb-api.com/app/fyp-bffpf/endpoint/locationDemo";
+	"https://eu-west-1.aws.data.mongodb-api.com/app/fyp-bffpf/endpoint/locationDemo";
 
-var geocodes = []
+var geocodes = [];
 fetch(webhook_url)
-.then(function (response) {
-	return response.json();
-})
-.then(function (data) {
-	// JSON.stringify(data, undefined, 2)
-	// 	.replace(/[&\\\#,+()$~%.'"*?<>{}]/g, "")
-	// 	.replace(/[\[\]']+/g, "")
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		// JSON.stringify(data, undefined, 2)
+		// 	.replace(/[&\\\#,+()$~%.'"*?<>{}]/g, "")
+		// 	.replace(/[\[\]']+/g, "")
 
-	// console.log(data[1]);
-	// for(x in data[1]) {
-	// 	console.log(x)
-	// }
+		// console.log(data[1]);
+		// for(x in data[1]) {
+		// 	console.log(x)
+		// }
 
-	for (var i = 0; i < data.length; i ++ ) {
-		let latitude = data[i].Latitude;
-		// console.log(latitude);
+		for (var i = 0; i < data.length; i++) {
+			let latitude = data[i].Latitude;
+			// console.log(latitude);
 
-		let longitude = data[i].Longitude;
-		// console.log(longitude);
-		geopin = [latitude, longitude ]
-		geocodes.push(geopin)
-	}
+			let longitude = data[i].Longitude;
+			// console.log(longitude);
+			geopin = [latitude, longitude];
+			geocodes.push(geopin);
+		}
 
-	console.log(geocodes);
-	setPins(geocodes);
-})
-.catch(function (err) {
-	console.log(err);
-});
-
-
+		console.log(geocodes);
+		setPins(geocodes);
+	})
+	.catch(function (err) {
+		console.log(err);
+	});
 
 // Initialize the map and set its options
 var map = L.map("map").setView([53.1424, -7.6921], 6); // Coords for Ireland
@@ -49,4 +46,3 @@ function setPins(geocodes) {
 		L.marker(geocode).addTo(map);
 	});
 }
-
