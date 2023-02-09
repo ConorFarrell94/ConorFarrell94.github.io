ca8afce1759742bccfde8a65da040c6bbe9ba700
Conor Farrell <conorfarrell187@gmail.com>
2023-02-09 18:51:26 +0000

    update map logic

--- js/map.js
+++ js/map.js
@@ -23,11 +23,12 @@ fetch(webhook_url)
 			let longitude = data[i].Longitude;
 			// console.log(longitude);
 			geopin = [latitude, longitude];
+			// L.marker(geopin).addTo(map);
 			geocodes.push(geopin);
 		}
 
-		console.log(geocodes);
-		setPins(geocodes);
+		// console.log(geocodes);
+		// setPins(geocodes);
 	})
 	.catch(function (err) {
 		console.log(err);
@@ -41,8 +42,56 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-function setPins(geocodes) {
-	geocodes.forEach(function (geocode) {
-		L.marker(geocode).addTo(map);
-	});
+
+var clickCircle;
+
+
+// function isMarkerInCircle(circle, mark) {
+// 	let circleCenter = circle.getLatLng();
+// 	geocodes.forEach(element =>
+// 		let markerPos = geopin.getLatLng();
+// 		if (circleCenter.distanceTo(markerPos) < circle.getRadius()) {
+// 			return true;
+// 		} )
+// }
+
+var markersLayer = new L.LayerGroup();
+function onMapClick(e) {
+	markersLayer.clearLayers();
+	markersLayer.addTo(map);
+	if (clickCircle != undefined) {
+		map.removeLayer(clickCircle);
+	}
+	clickCircle = L.circle(e.latlng, 20000, {
+		color: "#ff3b6b",
+		fillOpacity: 0.3,
+		opacity: 1,
+	}).addTo(map);
+	geocodes.forEach(async (geocode) => {
+		latlng_a = new L.LatLng(geocode[0], geocode[1])
+		if (latlng_a.distanceTo(e.latlng) < 20000) {
+			marker = L.marker(latlng_a);
+			markersLayer.addLayer(marker);
+		}
+	}
+	);
+
 }
+map.on("click", onMapClick);
+
+
+
+
+
+// map.on('click', function(e){
+// 	var coord = e.latlng;
+// 	var lat = coord.lat;
+// 	var lng = coord.lng;
+// 	console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
+// 	L.circle([lat, lng], {radius: 20000}).addTo(map);
+// 	});
+// function setPins(geocodes) {
+// 	geocodes.forEach(function (geocode) {
+// 		L.marker(geocode).addTo(map);
+// 	});
+// }
