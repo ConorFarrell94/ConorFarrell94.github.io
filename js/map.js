375c2dea9d9c76ff07c5365fc1564c97302bbf0b
Conor Farrell <conorfarrell187@gmail.com>
2023-03-15 13:54:40 +0000

    adding polygons to map
    TODO: youve to select the checkboxes twice for some reason on page load for it to work? fix it

--- js/map.js
+++ js/map.js
@@ -40,43 +40,157 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 function update() {
 	let x = document.getElementById("myRange").value;
-
 	document.getElementById("range").innerText = x + "km";
 }
 
 function onPinClick(e) {
 	alert(pin);
 }
-function onMapClick(e) {
-	var range = document.getElementById("myRange").value * 1000;
+
+var circleslayer;
+
+function clickCircle(e) {
 	markersLayer.clearLayers();
 	markersLayer.addTo(map);
-	if (clickCircle != undefined) {
-		map.removeLayer(clickCircle);
+
+	// Remove the existing circles layer
+	if (circleslayer) {
+		map.removeLayer(circleslayer);
 	}
-	clickCircle = L.circle(e.latlng, range, {
-		color: "#ff3b6b",
-		fillOpacity: 0.3,
-		opacity: 1,
-	}).addTo(map);
-	pinData.forEach(async (pin) => {
-		latlng_a = new L.LatLng(pin[4], pin[5]);
-		if (latlng_a.distanceTo(e.latlng) < range) {
-			marker = L.marker(latlng_a);
-			marker
-				.bindPopup(
-					"NAME : " +
-						pin[0] +
-						"<br> SURNAME : " +
-						pin[1] +
-						"<br> UNIT : " +
-						pin[2] +
-						"<br> RANK : " +
-						pin[3]
-				)
-				.openPopup();
-			markersLayer.addLayer(marker);
+	if (document.getElementById("checkbox1").checked) {
+		markersLayer.clearLayers();
+		circleslayer = new L.LayerGroup();
+		function onMapClick(e) {
+			var range = document.getElementById("myRange").value * 1000;
+			if (e && e.latlng) {
+				// clickCircle = L.circle(e.latlng, range, {
+				// 	color: "#ff3b6b",
+				// 	fillOpacity: 0.3,
+				// 	opacity: 1,
+				// }).addTo(map);
+
+				var circle = L.circle(e.latlng, range, {
+					color: "#ff3b6b",
+					fillOpacity: 0.3,
+					opacity: 1,
+				});
+				circleslayer.addLayer(circle);
+				circleslayer.addTo(map);
+
+				pinData.forEach((pin) => {
+					latlng_a = new L.LatLng(pin[4], pin[5]);
+					if (latlng_a.distanceTo(e.latlng) < range) {
+						marker = L.marker(latlng_a);
+						marker
+							.bindPopup(
+								"NAME : " +
+									pin[0] +
+									"<br> SURNAME : " +
+									pin[1] +
+									"<br> UNIT : " +
+									pin[2] +
+									"<br> RANK : " +
+									pin[3]
+							)
+							.openPopup();
+						markersLayer.addLayer(marker);
+					}
+				});
+			}
 		}
+
+		map.on("click", onMapClick);
+	} else {
+		map.off("click");
+	}
+}
+
+var polygonsLayer;
+
+function clickPolygon(e) {
+	markersLayer.clearLayers();
+	markersLayer.addTo(map);
+
+	// Remove the existing polygons layer
+	if (polygonsLayer) {
+		map.removeLayer(polygonsLayer);
+	}
+
+	if (document.getElementById("checkbox2").checked) {
+		var polygonCoords = [];
+		polygonsLayer = new L.LayerGroup();
+		var numPoints = 0;
+
+		function onMapClick(e) {
+			numPoints++;
+			polygonCoords.push([e.latlng.lat, e.latlng.lng]);
+
+			if (numPoints === 1) {
+				L.popup()
+					.setLatLng(e.latlng)
+					.setContent("Click two more points to make a polygon.")
+					.openOn(map);
+			} else if (numPoints === 2) {
+				L.popup()
+					.setLatLng(e.latlng)
+					.setContent("Click one more point to complete the polygon.")
+					.openOn(map);
+			} else if (numPoints === 3) {
+				var polygon = L.polygon(polygonCoords, {
+					color: "#36A8E0",
+					fillOpacity: 0.3,
+					opacity: 1,
+				});
+				polygonsLayer.addLayer(polygon);
+				polygonsLayer.addTo(map);
+				polygonCoords = [];
+				numPoints = 0;
+
+				pinData.forEach((pin) => {
+					var latlng = L.latLng(pin[4], pin[5]);
+					var polygonGeoJSON = polygon.toGeoJSON();
+					var pinGeoJSON = {
+						type: "Feature",
+						properties: {},
+						geometry: {
+							type: "Point",
+							coordinates: [pin[5], pin[4]],
+						},
+					};
+					if (turf.booleanPointInPolygon(pinGeoJSON, polygonGeoJSON)) {
+						var marker = L.marker(latlng);
+						marker
+							.bindPopup(
+								"NAME : " +
+									pin[0] +
+									"<br> SURNAME : " +
+									pin[1] +
+									"<br> UNIT : " +
+									pin[2] +
+									"<br> RANK : " +
+									pin[3]
+							)
+							.openPopup();
+						markersLayer.addLayer(marker);
+					}
+				});
+			}
+		}
+
+		map.on("click", onMapClick);
+	} else {
+		map.off("click");
+	}
+}
+
+map.on("click", clickPolygon);
+
+function clearMap() {
+	map.eachLayer(function (layer) {
+		map.removeLayer(layer);
+		L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			attribution:
+				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		}).addTo(map);
 	});
 }
-map.on("click", onMapClick);
